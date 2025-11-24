3147a1302f695aceddd611983f702012d52ee386
choiyoonhyeok <midascys2@naver.com>
2025-11-24 19:53:32 +0900

    Update __init__.py

--- src/ood_sentiment_llm/embedding/__init__.py
+++ src/ood_sentiment_llm/embedding/__init__.py
@@ -1,11 +1,15 @@
+# src/ood_sentiment_llm/embedding/__init__.py
+
 from .openai_embedding import (
     truncate_by_tokens,
     embed_texts,
     build_pn_ood_embeddings_from_jsonl,
+    main as run_openai_embedding,
 )
 
 __all__ = [
     "truncate_by_tokens",
     "embed_texts",
     "build_pn_ood_embeddings_from_jsonl",
+    "run_openai_embedding",
 ]
