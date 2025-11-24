af3cd54715f8a622301869a8bff8814b38bcb0c3
choiyoonhyeok <midascys2@naver.com>
2025-11-24 19:10:56 +0900

    Update __init__.py

--- src/ood_sentiment_llm/models/__init__.py
+++ src/ood_sentiment_llm/models/__init__.py
@@ -1,3 +1,10 @@
 from .heads import ProjectionHead, ClassifierHead
+from .pn_train import main as train_pn
+from .pn_eval import main as eval_pn
 
-__all__ = ["ProjectionHead", "ClassifierHead"]
+__all__ = [
+    "ProjectionHead",
+    "ClassifierHead",
+    "train_pn",
+    "eval_pn",
+]
