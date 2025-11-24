d2478bbcdd3c99ff9a1f8b93e6fa9296e7bb2631
choiyoonhyeok <midascys2@naver.com>
2025-11-24 13:19:54 +0900

    Update __init__.py

--- src/ood_sentiment_llm/__init__.py
+++ src/ood_sentiment_llm/__init__.py
@@ -1,1 +1,12 @@
+"""
+ood_sentiment_llm
+=================
 
+LLM 임베딩 기반 감성 분류 + OOD 검출 프로젝트용 패키지.
+"""
+
+from . import config
+from . import utils
+from . import ood
+
+__all__ = ["config", "utils", "ood"]
